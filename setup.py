86ddff3f395085d9b253e406cb4ff328355673e2
Michael Howitz <mh@gocept.com>
2009-01-04 21:09:04 +0000

    vb

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def read(*rnames):
 
 setup (
     name='z3c.authenticator',
-    version='0.6.0dev',
+    version='0.6.0',
     author = "Roger Ineichen and the Zope Community",
     author_email = "zope-dev@zope.org",
     description = "IAuthentication implementation for for Zope3",
