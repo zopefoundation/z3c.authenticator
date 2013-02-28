6a77e7f6fbf37c7b2dacd497d8bb57a83f8cdab0
Albertas Agejevas <alga@pov.lt>
2013-02-28 20:43:52 +0200

    Preparing release 1.0.0a1

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def read(*rnames):
 
 setup (
     name='z3c.authenticator',
-    version='0.8.2dev',
+    version='1.0.0a1',
     author = "Roger Ineichen and the Zope Community",
     author_email = "zope-dev@zope.org",
     description = "IAuthentication implementation for for Zope3",
