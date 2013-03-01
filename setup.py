3b88305f679776763416f6aa3336129207e0beb8
Stephan Richter <stephan.richter@gmail.com>
2013-02-28 23:45:36 -0500

    Back to development: 1.0.0a5

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ def read(*rnames):
 
 setup (
     name='z3c.authenticator',
-    version='1.0.0a4',
+    version='1.0.0a5.dev0',
     author = "Roger Ineichen and the Zope Community",
     author_email = "zope-dev@zope.org",
     description = "IAuthentication implementation for for Zope3",
