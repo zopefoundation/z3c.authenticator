ce357a01b97fb2986ff0c9c8637dba1bcdffc834
Michael Howitz <mh@gocept.com>
2023-02-08 16:51:05 +0100

    Bumped version for breaking release.

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def read(*rnames):
 
 setup(
     name='z3c.authenticator',
-    version='1.1.0.dev0',
+    version='2.0.dev0',
     author="Roger Ineichen and the Zope Community",
     author_email="zope-dev@zope.org",
     description="IAuthentication implementation for for Zope3",
